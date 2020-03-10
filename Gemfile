5f4c5eecf1fac5e7a8dfd68e6f66fb50685135d6
sheepdog0820 <sheepdgo1919@gmail.com>
2020-03-10 21:30:37 +0900

    user-login vol.2 ログイン画面作成
    ログイン画面作成
    ファイル選択項目追加
    リサイズ用のメソッドを設定

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'font-awesome-sass'
 gem 'devise'
 gem 'haml-rails'
+gem 'mini_magick'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  def thumbnail
+    return self.image.variant(resize: '100x100')
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
